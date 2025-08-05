1eda8a12427022afab30c71000d7ee441cc724dc
FELIPE VIEIRA DOS SANTOS <24122377@dsn127m1249387.cfp127aluno.local>
2025-08-05 13:49:41 -0300

    inicio app de clientes

---
+++ app/src/main/java/br/senai/sp/jandira/clientesapp/service/ClienteService.kt
@@ -0,0 +1,23 @@
+package br.senai.sp.jandira.clientesapp.service
+
+import br.senai.sp.jandira.clientesapp.model.Cliente
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface ClienteService {
+
+    @POST("clientes")
+    fun cadastrarCliente(@Body cliente: Cliente): Call<Cliente>
+
+    @GET("clientes")
+    fun listarClientes(): Call<List<Cliente>>
+
+    @GET("clientes/{id}")
+    
+    fun listarClientes(@Path("id") id: Long): Call<List<Cliente>>
+
+}
